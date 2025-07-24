8e1d19e39e9dc9318ee602dae1bbc2d1d8bbf8bf
ErChoi-com <ernestljchoi@gmail.com>
2025-07-24 00:13:24 -0400

    git proving grounds

--- interaction.js
+++ interaction.js
@@ -36,11 +36,9 @@ make.addEventListener("click", () => {
         peer.on("connection", (incomingConn) => {
             const peerId = incomingConn.peer;
             connections[peerId] = incomingConn;
-            // where block of code originally was
-            incomingConn.on("data", (data) => {
-                // dropped block of code
-                if (data.type === "intro") {
-                    for (let id in usernames) {
+
+            /* incomingConn.on("data", (data) => {
+                for (let id in usernames) {
                     if (data.username === usernames[id]) {
                         incomingConn.send({
                             type: "error",
@@ -55,7 +53,8 @@ make.addEventListener("click", () => {
                         return;
                     }
                 }
-                // moved block of code
+                    */
+                if (data.type === "intro") {
                     // Save username from the peer
                     usernames[peerId] = data.username;
                     attachMessage(`${data.username} joined the room.`);
